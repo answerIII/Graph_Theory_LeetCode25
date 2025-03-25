1f27304c3e9eb23eefd72c55b7bdef6520a6cabf
Maria Mikhailova <mariiemik@gmail.com>
2025-03-26 01:42:00 +0300

    Accepted,  Runtime 41 ms Beats 70.82% ,  Memory 67.84 MB Beats 83.79%

--- 1466.java
+++ 1466.java
@@ -1,7 +1,5 @@
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 
 class Solution {
@@ -9,68 +7,16 @@ class Solution {
     private List<Integer>[] graph;
     private boolean[] visited;
     private int count;
-    private int current_root;
-    int[] roots;
 
     public int minReorder(int n, int[][] connections) {
         count = 0;
         graph = adjencyListAndInvert(connections, n);
         visited = new boolean[n];
         count = 0;
-        List<Integer> roots_list = new ArrayList<>(); //сохраняем корни деревьев обхода
-        roots = new int[n];
-        Arrays.fill(roots, -1);
-        for (int i = 0; i < n; ++i) {
-            if (!visited[i]) {
-                current_root = i;
-                dfs(i);
-                if (i != 0) {
-                    roots_list.add(i);
-                }
 
-            }
-        }
-        //То что не соединено с 0 напрямую, соединяем через другие деревья
-        while (!roots_list.isEmpty()) {
-            Iterator<Integer> iterator = roots_list.iterator();
-            while (iterator.hasNext()) {
-                int r = iterator.next();
-                boolean removed = false; // Флаг, показывающий, удалён ли элемент
-                for (int v : graph[r]) {
-                    if (roots[v] == 0) {
-                        ++count;
-                        roots[r] = 0;
-                        if (!removed) {
-                            iterator.remove(); // Удаляет элемент со значением 2
-                            removed = true;
-                        }
-                        if (graph[r].size() > 1 && roots[graph[r].get(1)] == r) {
-                            for (int s = 0; s < n; ++s) {
-                                if (roots[s] == r) {
-                                    roots[s] = 0;
-                                }
-                            }
-                            break;
-                        }
-
-                    } else if (roots[v] != r) {
-                        roots[v] = roots[r];
-                        roots_list.remove(Integer.valueOf(v)); // Удаляет ПЕРВОЕ вхождение числа 2
-                        if (graph[r].size() > 1) {
-
-                            for (int s = 0; s < n; ++s) {
-                                if (roots[s] == v) {
-                                    roots[s] = roots[r];
-                                }
-                            }
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-
+        dfs(0);
         return count;
+
     }
 
     static List<Integer>[] adjencyListAndInvert(int[][] edges, int n) {
@@ -81,6 +27,8 @@ class Solution {
         for (int[] edge : edges) {
             //inverting
             adj_list[edge[1]].add(edge[0]);
+            adj_list[edge[0]].add(-edge[1]);
+
         }
         return adj_list;
 
@@ -91,9 +39,15 @@ class Solution {
             return;
         }
         visited[node] = true;
-        roots[node] = current_root;
         for (int u : graph[node]) {
-            dfs(u);
+            if (u < 0 && !visited[-u]) {
+                ++count;
+                dfs(-u);
+
+            } else if (u > 0 && !visited[u]) {
+                dfs(u);
+            }
+
         }
     }
 
