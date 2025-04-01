bf6c67cf6fa931edcd328d09f7ef46041a13cfe6
nomatterwhoisthat <nurkakabaeva@gmail.com>
2025-04-01 23:36:08 +0300

    1129

--- src/1129.cpp
+++ src/1129.cpp
@@ -7,18 +7,18 @@ public:
         std::vector<std::vector<std::pair<int, int>>> graph(n);
 
         for(int i = 0; i < redEdges.size(); ++i){
-            graph[redEdges[i][0]].emplace_back(redEdges[i][1], 0); 
+            graph[redEdges[i][0]].push_back({redEdges[i][1], 0}); 
         }
 
         for(int i = 0; i < blueEdges.size(); ++i){
-            graph[blueEdges[i][0]].emplace_back(blueEdges[i][1], 1);  
+            graph[blueEdges[i][0]].push_back({blueEdges[i][1], 1});  
         }
 
         std::queue<std::pair<int, int>> q;
         q.push(std::make_pair(0, 0));  
         q.push(std::make_pair(0, 1)); 
 
-        std::vector<std::vector<int>> dist(n, std::vector<int>(2, -1));
+        std::vector<std::vector<int>> dist(n, std::vector<int>(2, INT_MAX));
         dist[0][0] = 0;
         dist[0][1] = 0;
 
@@ -33,7 +33,7 @@ public:
                 int neighbor = graph[node][i].first;
                 int edgeColor = graph[node][i].second;
 
-                if(edgeColor == next_color && dist[neighbor][edgeColor] == -1){
+                if(edgeColor == next_color && dist[neighbor][edgeColor] == INT_MAX){
                     dist[neighbor][edgeColor] = 1 + dist[node][color];
                     q.push(std::make_pair(neighbor, edgeColor));
                 }
@@ -43,11 +43,11 @@ public:
         std::vector<int> result(n, -1);
 
         for(int i = 0; i < n; ++i){
-            if(dist[i][0] != -1 && dist[i][1] != -1){
+            if(dist[i][0] != INT_MAX && dist[i][1] != INT_MAX){
                 result[i] = std::min(dist[i][0], dist[i][1]);
-            } else if(dist[i][0] != -1){
+            } else if(dist[i][0] != INT_MAX){
                 result[i] = dist[i][0];
-            } else if(dist[i][1] != -1){
+            } else if(dist[i][1] != INT_MAX){
                 result[i] = dist[i][1];
             }
         }
