46c0f46a5d5c11054319ee272c013e4771d79936
nomatterwhoisthat <nurkakabaeva@gmail.com>
2025-04-01 23:19:30 +0300

    3286

--- src/3286.cpp
+++ src/3286.cpp
@@ -1,56 +1,47 @@
 #include <vector>
 #include <queue>
-#include <tuple>
-
 
 class Solution {
 public:
-    bool findSafeWalk(vector<vector<int>>& grid, int health) {
-        int total_cols = grid[0].size();
+    bool findSafeWalk(std::vector<std::vector<int>>& grid, int health) {
         int total_rows = grid.size();
-
+        int total_cols = grid[0].size();
+        std::vector<std::vector<int>> dist(total_rows, std::vector<int>(total_cols, INT_MAX));
+        
+        dist[0][0] = grid[0][0];
+        std::queue<std::pair<int, int>> q;
+        q.emplace(0, 0);
+        
         std::vector<std::pair<int, int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        std::queue<std::tuple<int, int, int>> q; // row, col, health
-        std::vector<std::tuple<int, int, int>> visited;
-
-        q.push({0,0, health});
-        visited.push_back({0, 0, health});
-
-        while(!q.empty()){
-            int x = std::get<0>(q.front());
-            int y = std::get<1>(q.front());
-            int h = std::get<2>(q.front());
-
+        
+        while (!q.empty()) {
+            int x = q.front().first;
+            int y = q.front().second;
             q.pop();
-
-            if(x == total_rows-1 && y == total_cols-1 && h >= 1){
-                return true;
-            }
-
-            for(int i = 0; i < directions.size(); ++i){
-                int nx = x + directions[i].first;
+            
+            for (int i = 0; i < directions.size(); ++i) {
+                int nx = x +  directions[i].first;;
                 int ny = y + directions[i].second;
-
-                if(nx >= 0 && nx < total_rows && ny >= 0 && ny < total_cols){
-                    int new_health = h - grid[nx][ny];
-
-                    // если здоровье остается положительным и клетка не была посещена
-                    bool is_visited = false;
-                    for (int j = 0; j < visited.size(); ++j) {
-                        if (std::get<0>(visited[j]) == nx && std::get<1>(visited[j]) == ny && std::get<2>(visited[j]) == new_health) {
-                            is_visited = true;
-                            break;
-                        }
-                    }
-
-                    if (new_health > 0 && !is_visited) {
-                        q.push(std::make_tuple(nx, ny, new_health)); 
-                        visited.push_back({nx, ny, new_health}); 
+                
+                
+                if (nx >= 0 && nx < total_rows && ny >= 0 && ny < total_cols) {
+                    int new_health = dist[x][y] + grid[nx][ny];
+                    
+                    if (new_health < dist[nx][ny]) {
+                        dist[nx][ny] = new_health;
+                        q.emplace(nx, ny);
                     }
                 }
             }
         }
+        
+        bool ans = false;
 
-        return false; 
+        if(dist[total_rows - 1][total_cols - 1] < health){
+            ans = true;
+        }
+
+        return ans;
     }
 };
+
