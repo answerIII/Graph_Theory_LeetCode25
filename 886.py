601d1263bd6f504f003d880b5de4d3ab07ca910c
Canis1219 <svetochka.kalinkina@mail.ru>
2025-04-11 17:28:48 +0300

    886_v9_Runtime Error

--- 886.py
+++ 886.py
@@ -1,13 +1,13 @@
 from typing import List
 
 class Solution:
-    def DFS(self, сurrent_node, graph, state, color) -> bool:
-            state[сurrent_node] = color
+    def DFS(self, current_node, graph, state, color) -> bool:
+            state[current_node] = color
             for neighbor in graph[current_node]:
-                        if state[neighbor] == color:
-                            return False
-                        if state[neighbor] is None and self.DFS(neighbor, graph, state, 1 - color) == False:
-                            return False
+                if state[neighbor] == color:
+                    return False
+                if state[neighbor] is None and self.DFS(neighbor, graph, state, 1 - color) == False:
+                return False
             return True
 
     def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
@@ -16,8 +16,8 @@ class Solution:
             graph[a - 1].append(b - 1)
             graph[b - 1].append(a - 1)
         state = [None] * n
-        for сurrent_node in range(n):
-            if state[сurrent_node] is None:
-                if self.DFS(сurrent_node, graph, state, 0) == False:
+        for current_node in range(n):
+            if state[current_node] is None:
+                if self.DFS(current_node, graph, state, 0) == False:
                     return False
         return True
