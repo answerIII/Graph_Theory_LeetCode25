ed567a36928a1abf8f7b6a382b831a7476ab1db5
Danil Mikhaylov <damikh13@gmail.com>
2025-04-17 01:43:21 +0300

    2316: dfs now returns size of component and doesn't remember exact vertices

--- 2316.py
+++ 2316.py
@@ -1,13 +1,14 @@
 class Solution(object):
     @staticmethod
-    def dfs(curr_vert, adjacency_list, is_visited, curr_conn_comp):
+    def run_dfs_and_return_size_of_conn_component(curr_vert, adjacency_list, is_visited):
         is_visited[curr_vert] = True
-
-        curr_conn_comp.append(curr_vert)
+        size = 1
 
         for adj_vertex in adjacency_list[curr_vert]:
             if not is_visited[adj_vertex]:
-                Solution.dfs(adj_vertex, adjacency_list, is_visited, curr_conn_comp)
+                size += Solution.run_dfs_and_return_size_of_conn_component(adj_vertex, adjacency_list, is_visited)
+        
+        return size
 
     def countPairs(self, n, edges):
         """
@@ -22,17 +23,13 @@ class Solution(object):
             adjacency_list[v].append(u)
 
         is_visited = [False] * n # for each vertex: True/False
-        all_conn_comps = []
+        all_conn_comps_sizes = []
 
         # find all connected components
         for curr_vertex in range(n):
             if not is_visited[curr_vertex]:
-                curr_conn_comp = []
-                Solution.dfs(curr_vertex, adjacency_list, is_visited, curr_conn_comp)
-                all_conn_comps.append(curr_conn_comp)
-
-        # as we don't need vertices in components (only their sizes to say [3] -- [5] <- 3*5 pairs), we'll store just sizes
-        all_conn_comps_sizes = [len(curr_conn_comp) for curr_conn_comp in all_conn_comps]
+                curr_conn_comp_size = Solution.run_dfs_and_return_size_of_conn_component(curr_vertex, adjacency_list, is_visited)
+                all_conn_comps_sizes.append(curr_conn_comp_size)
 
         total_unreachable_pairs = 0
         for i in range(len(all_conn_comps_sizes)):
