29a1728f8279238b0dec230e9a3fea17082f226b
Canis1219 <svetochka.kalinkina@mail.ru>
2025-04-17 00:12:21 +0300

    1462_v3_Wrong Answer

--- 1462.py
+++ 1462.py
@@ -2,45 +2,39 @@ from typing import List
 from collections import defaultdict
 
 class Solution:
-    def DFS(self, course, visited, res, course_map):
-        if visited[course] == 0:
-            visited[course] = 1
-            
-            for next_course in course_map[course]:
-                if self.DFS(next_course, visited, res, course_map) == False:
-                    return False
-                    
-            res.append(course)
-            visited[course] = 2
-            return True
-            
-        if visited[course] == 1: #нашли цикл
-            return False
-            
-        if visited[course] == 2:
-            return True
+    def DFS(self, course, visited, component, course_map):
+        visited[course] = 1
+        component.append(course)
+        
+        for next_course in course_map[course]:
+            if visited[next_course] == 0: 
+                self.DFS(next_course, visited, component, course_map)
 
     def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
         course_map = defaultdict(list)
         for course, prereq in prerequisites:
-            course_map[course].append(prereq)  
-
-
-        if not prerequisites:
-            return [False] * len(queries)
+            course_map[course].append(prereq)
         
-        visited = [0] * numCourses  # 0-не трогали, 2-идем по цепочке, 3-уже прошли
-        res = []
+        visited = [0] * numCourses  # 0- не посещали, 1- посещена
+        components = []
+        course_to_component = {} 
 
         for course in range(numCourses):
             if visited[course] == 0:
-                if self.DFS(course, visited, res, course_map) == False:
-                    return []
-        res.reverse()
+                component = []
+                self.DFS(course, visited, component, course_map)
+                components.append(component)  
 
-        course_to_index = {course: idx for idx, course in enumerate(res)}
+                for c in component:
+                    course_to_component[c] = len(components) - 1
+        
         answer = []
         for uj, vj in queries:
-            answer.append(course_to_index[uj] < course_to_index[vj])
-
+            uj_component_idx=course_to_component[uj]
+            vj_component_idx=course_to_component[vj]
+            if  uj_component_idx != vj_component_idx:
+                answer.append(False)
+            else:
+                answer.append(components[uj_component_idx].index(uj) < components[vj_component_idx].index(vj))
+        
         return answer
