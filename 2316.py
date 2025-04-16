c6e88c9b99d10d065525b1836c9706d391140ebd
Danil Mikhaylov <damikh13@gmail.com>
2025-04-17 00:39:31 +0300

    2316: implemented DFS -> list all connected components (each el contains all vertices in this component)

--- 2316.py
+++ 2316.py
@@ -1,4 +1,14 @@
 class Solution(object):
+    @staticmethod
+    def dfs(curr_vert, adjacency_list, is_visited, curr_conn_comp):
+        is_visited[curr_vert] = True
+
+        curr_conn_comp.append(curr_vert)
+
+        for adj_vertex in adjacency_list[curr_vert]:
+            if not is_visited[adj_vertex]:
+                Solution.dfs(adj_vertex, adjacency_list, is_visited, curr_conn_comp)
+
     def countPairs(self, n, edges):
         """
         :type n: int
@@ -10,3 +20,13 @@ class Solution(object):
         for u, v in edges:
             adjacency_list[u].append(v)
             adjacency_list[v].append(u)
+
+        is_visited = [False] * n # for each vertex: True/False
+        all_conn_comps = []
+
+        # find all connected components
+        for curr_vertex in range(n):
+            if not is_visited[curr_vertex]:
+                curr_conn_comp = []
+                Solution.dfs(curr_vertex, adjacency_list, is_visited, curr_conn_comp)
+                all_conn_comps.append(curr_conn_comp)
