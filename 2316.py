fbc20e541ebf461d3d7a3c87b589f1a791217785
Danil Mikhaylov <damikh13@gmail.com>
2025-04-17 00:37:32 +0300

    2316: initial input -> adjacency list

--- 2316.py
+++ 2316.py
@@ -5,3 +5,8 @@ class Solution(object):
         :type edges: List[List[int]]
         :rtype: int
         """
+
+        adjacency_list = [[] for i in range(n)] # for i-th vertex: [v0, v4, ..., v.] <- all vertices adjacent to vi
+        for u, v in edges:
+            adjacency_list[u].append(v)
+            adjacency_list[v].append(u)
