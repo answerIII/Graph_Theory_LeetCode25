51ed941505da3e6d1120f91a7ddaf00d7cf5ca66
Canis1219 <svetochka.kalinkina@mail.ru>
2025-04-17 16:33:28 +0300

    1462_Accepted. Runtime: Beats 69.74%, Memory: Beats 43.95%

--- 1462.py
+++ 1462.py
@@ -2,39 +2,29 @@ from typing import List
 from collections import defaultdict
 
 class Solution:
-    def DFS(self, course, visited, component, course_map):
+    def DFS(self, course, visited, graph):
         visited[course] = 1
-        component.append(course)
-        
-        for next_course in course_map[course]:
-            if visited[next_course] == 0: 
-                self.DFS(next_course, visited, component, course_map)
+        for next_course in graph[course]:
+            if visited[next_course] == 0:
+                self.DFS(next_course, visited, graph)
 
     def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
-        course_map = defaultdict(list)
-        for course, prereq in prerequisites:
-            course_map[prereq].append(course)
-        
-        visited = [0] * numCourses  # 0- не посещали, 1- посещена
-        components = []
-        course_to_component = {} 
+        graph = defaultdict(list)
+        for prereq, course in prerequisites:
+            graph[prereq].append(course)
+
+        # reachable[i][j] = true (если из i можно добраться до j)
+        reachable = [[False] * numCourses for _ in range(numCourses)]
 
         for course in range(numCourses):
-            if visited[course] == 0:
-                component = []
-                self.DFS(course, visited, component, course_map)
-                components.append(component)  
+            visited = [0] * numCourses
+            self.DFS(course, visited, graph)
+            for i in range(numCourses):
+                if visited[i]:
+                    reachable[course][i] = True
 
-                for c in component:
-                    course_to_component[c] = len(components) - 1
-        
         answer = []
         for uj, vj in queries:
-            uj_component_idx=course_to_component[uj]
-            vj_component_idx=course_to_component[vj]
-            if  uj_component_idx != vj_component_idx:
-                answer.append(False)
-            else:
-                answer.append(components[uj_component_idx].index(uj) < components[vj_component_idx].index(vj))
+            answer.append(reachable[uj][vj])
         
         return answer
