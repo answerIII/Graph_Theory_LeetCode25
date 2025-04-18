21c3bcbd039b4e7e1633f1c36c0ee7cc6e4fb892
Canis1219 <svetochka.kalinkina@mail.ru>
2025-04-18 16:42:39 +0300

    3286_v1_Wrong Answer

---
+++ 3286.py
@@ -0,0 +1,53 @@
+from collections import deque
+from typing import List
+
+class Solution:
+    def findSafeWalk(self, grid: List[List[int]], health: int) -> bool:
+        
+        m = len(grid)
+        n = len(grid[0])
+        visited = [[False] * n for _ in range(m)]
+        queue = deque() #BFS
+        queue.append((0, 0, health)) #row,col,health
+        visited[0][0] = True
+
+        while queue:
+            row, col, health = queue.popleft()
+
+            if (row - 1 >= 0) and (visited[row - 1][col] == False): #вверх
+                if grid[row - 1][col] == 1:
+                    health -= 1
+
+                if health >= 1:
+                    visited[row - 1][col] = True
+                    queue.append((row - 1, col, health))
+
+            if (row + 1 < m) and (visited[row + 1][col] == False): #вниз
+                if grid[row + 1][col] == 1:
+                    health -= 1
+
+                if health >= 1:
+                    visited[row + 1][col] = True
+                    queue.append((row + 1, col, health))
+
+            if (col - 1 >= 0) and (visited[row][col - 1] == False): #влево
+                if grid[row][col - 1] == 1:
+                    health -= 1
+
+                if health >= 1:
+                    visited[row][col - 1] = True
+                    queue.append((row, col - 1, health))
+
+            if (col + 1 < n) and (visited[row][col + 1] == False): #вправо
+                if grid[row][col + 1] == 1:
+                    health -= 1
+
+                if health >= 1:
+                    visited[row][col - 1] = True
+                    queue.append((row, col - 1, health))
+
+            if (row == m - 1) and (col == n - 1): 
+                if health >= 1:
+                    return True
+                else:
+                    return False
