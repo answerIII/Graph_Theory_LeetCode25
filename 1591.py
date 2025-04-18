4d41eb0162e5cf275cadacff6d8362b655e5bd75
Canis1219 <svetochka.kalinkina@mail.ru>
2025-04-18 14:02:07 +0300

    1591_v1_Wrong Answer

---
+++ 1591.py
@@ -0,0 +1,38 @@
+from typing import List
+
+class Solution:
+    def isPrintable(self, targetGrid: List[List[int]]) -> bool:
+        m = len(targetGrid)
+        n = len(targetGrid[0])
+        
+        colors = set()
+        for i in range(m):
+            for j in range(n):
+                colors.add(targetGrid[i][j])
+
+        borders = {}
+        for i in range(m):
+            for j in range(n):
+                current_color = targetGrid[i][j]
+                
+                if current_color not in borders:
+                    borders[current_color] = [i, j, i, j]
+                else:
+                    if i < borders[current_color][0]: #минимумы
+                        borders[current_color][0] = i  
+                    if j < borders[current_color][1]:
+                        borders[current_color][1] = j 
+
+                    if i > borders[current_color][2]: #максимумы
+                        borders[current_color][2] = i
+                    if j > borders[current_color][3]:
+                        borders[current_color][3] = j
+
+        for color in borders:
+            x1, y1, x2, y2 = borders[color]
+            
+            for i in range(x1, x2):
+                for j in range(y1, y2):
+                    if targetGrid[i][j] < color:
+                        return False  #из предположения, что с каждым разом мы увеличиваем значение цвета
+        return True
