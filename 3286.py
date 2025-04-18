1e310563974ec9f344f56419fab4c408488d4723
Canis1219 <svetochka.kalinkina@mail.ru>
2025-04-18 23:11:58 +0300

    3286_Accepted. Runtime: Beats 52.59%, Memory: Beats 91.83%

--- 3286.py
+++ 3286.py
@@ -3,49 +3,37 @@ from typing import List
 
 class Solution:
     def findSafeWalk(self, grid: List[List[int]], health: int) -> bool:
-        
+
         m = len(grid)
         n = len(grid[0])
+        max_health = [[0] * n for _ in range(m)]
+
         queue = deque() #BFS
-        queue.append((0, 0, health - 1 if grid[0][0] == 1 else health)) #row,col,health
+
+        queue.append((0, 0, health)) #row,col,health
 
         while queue:
             row, col, health = queue.popleft()
+            
+            if grid[row][col] == 1:
+                health -= 1
 
+            if health <= 0 or max_health[row][col] >= health:
+                continue
+           
+            max_health[row][col] = health
+            
             if (row == m - 1) and (col == n - 1):
                 if health >= 1:
                     return True
 
             if (row - 1 >= 0): #вверх
-                if grid[row - 1][col] == 1:
-                    new_health = health - 1
-                else:
-                    new_health = health
-
-                if new_health >= 1:
-                    queue.append((row - 1, col, new_health))
-
+                queue.append((row - 1, col, health))
             if (row + 1 < m): #вниз
-                if grid[row + 1][col] == 1:
-                    new_health = health - 1
-                else:
-                    new_health = health
-                if  new_health >= 1:
-                    queue.append((row + 1, col, new_health))
-
+                queue.append((row + 1, col, health))
             if (col - 1 >= 0): #влево
-                if grid[row][col - 1] == 1:
-                    new_health = health - 1
-                else:
-                    new_health = health
-                if  new_health >= 1:
-                    queue.append((row, col - 1, new_health))
-
+                queue.append((row, col - 1, health))
             if (col + 1 < n): #вправо
-                if grid[row][col + 1] == 1:
-                    new_health = health - 1
-                else:
-                    new_health = health
-                if  new_health >= 1:
-                    queue.append((row, col + 1, new_health))
+                queue.append((row, col + 1, health))
+        
         return False
