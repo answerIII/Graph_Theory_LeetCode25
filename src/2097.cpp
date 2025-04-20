ce7fa3343a484caaad8b651f224da3b9436646ad
nomatterwhoisthat <116099741+nomatterwhoisthat@users.noreply.github.com>
2025-04-21 00:51:28 +0300

    2097 - не проходит второй тест, так как результат неверный

---
+++ src/2097.cpp
@@ -0,0 +1,43 @@
+#include <unordered_map>
+#include <vector>
+#include <deque>
+#include <algorithm>
+
+class Solution {
+public:
+    void dfs(int node, std::unordered_map<int, std::deque<int>>& graph, std::vector<std::vector<int>>& result) {
+        while (!graph[node].empty()) {
+            int neighbour = graph[node].front();
+            graph[node].pop_front();
+            dfs(neighbour, graph, result);
+            result.push_back({ node, neighbour });
+        }
+    }
+
+    std::vector<std::vector<int>> validArrangement(std::vector<std::vector<int>>& pairs) {
+        std::unordered_map<int, std::deque<int>> graph;
+        std::unordered_map<int, int> in_degree, out_degree;
+
+        for (int i = 0; i < pairs.size(); ++i) {
+            int start = pairs[i][0];
+            int end = pairs[i][1];
+            graph[start].push_back(end);
+            out_degree[start]++;
+            in_degree[end]++;
+        }
+
+        int start_node = 0; 
+        for (auto& entry : graph) {
+            int node = entry.first;
+            if (out_degree[node] > in_degree[node]) {
+                start_node = node;
+                break;
+            }
+        }
+
+        std::vector<std::vector<int>> result;
+        dfs(start_node, graph, result);
+        std::reverse(result.begin(), result.end());
+        return result;
+    }
+};
