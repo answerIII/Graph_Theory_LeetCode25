b144e411b41b2532531c59baa49beae7c68febbc
denischumak <86205264+denischumak@users.noreply.github.com>
2025-04-21 00:40:30 +0300

    2316.cpp
    another aproach using DSU

--- 2316.cpp
+++ 2316.cpp
@@ -1,32 +1,39 @@
 class Solution {
 public:
 
-    void DFS(int u, std::vector<std::vector<int>>& adjacency, std::vector<bool>& visited, size_t& k) {
-        ++k;
-        visited[u] = true;
-        for (auto v : adjacency[u]) {
-            if (!visited[v]) {
-                DFS(v, adjacency, visited, k);
-            }
+    int leader(std::vector<int>& p, int v) {
+        return (v == p[v]) ? v : p[v] = leader(p, p[v]);
+    }
+    void unite(std::vector<int>& p, std::vector<int>& s, int a, int b) {
+        a = leader(p, a);
+        b = leader(p, b);
+        if (a == b) {
+            return;
         }
+        if (s[a] > s[b]) {
+            std::swap(a, b);
+        }
+        s[b] += s[a];
+        p[a] = b;
     }
 
     long long countPairs(int n, std::vector<std::vector<int>>& edges) {
-        std::vector<std::vector<int>> adjacency(n);
-        for (int i = 0; i < edges.size(); ++i) {
-            adjacency[edges[i][0]].push_back(edges[i][1]);
-            adjacency[edges[i][1]].push_back(edges[i][0]);
-        }
-        std::vector<bool> visited(n, false);
-        size_t result = static_cast<size_t>(n) * (n - 1) / 2;
-        size_t k;
+        std::vector<int> p(n);
+        std::vector<int> s(n);
         for (int i = 0; i < n; ++i) {
-            k = 0;
-            if (!visited[i]) {
-                DFS(i, adjacency, visited, k);
+            p[i] = i;
+            s[i] = 1;
+        }
+        for (int i = 0; i < edges.size(); ++i) {
+            unite(p, s, edges[i][0], edges[i][1]);
+        }
+        std::unordered_set<int> components;
+        size_t result = static_cast<size_t>(n) * (n - 1) / 2;
+        for (int i = 0; i < n; ++i) {
+            auto it = components.insert(leader(p, i));
+            if (it.second) {
+                result -= static_cast<size_t>(s[*it.first]) * (s[*it.first] - 1) / 2;
             }
-            result -= k * (k - 1) / 2;
         }
         return result;
     }
-};
