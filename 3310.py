8561af4c943d893d86c98d6c2358e51a131aacdb
Danil Mikhaylov <damikh13@gmail.com>
2025-04-21 21:41:57 +0300

    3310: intial code template

---
+++ 3310.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def remainingMethods(self, n, k, invocations):
+        """
+        :type n: int
+        :type k: int
+        :type invocations: List[List[int]]
+        :rtype: List[int]
+        """
