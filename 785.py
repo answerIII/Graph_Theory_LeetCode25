657c3cc95bd3146b8708dbb31fc1b7d318845acb
Danil Mikhaylov <damikh13@gmail.com>
2025-04-21 23:51:20 +0300

    785: + DFS that tries to color with 2 colors (if not possible, return False)

--- 785.py
+++ 785.py
@@ -1,4 +1,22 @@
 class Solution(object):
+    @staticmethod
+    def run_dfs_and_color_vertices(curr_vert, adjacency_list, coloring):
+        coloring[curr_vert] = 1
+
+        stack = [curr_vert]
+
+        while len(stack) > 0:
+            curr_vert = stack.pop()
+
+            for adj_vertex in adjacency_list[curr_vert]:
+                if coloring[adj_vertex] == 0:
+                    coloring[adj_vertex] = -coloring[curr_vert]
+                    stack.append(adj_vertex)
+                elif coloring[adj_vertex] == coloring[curr_vert]:
+                    return False
+        return True
+
+
     def isBipartite(self, graph):
         """
         :type graph: List[List[int]]
