e39c9501d94a51c233ae95f9173f8beb96770f2e
Danil Mikhaylov <damikh13@gmail.com>
2025-04-21 22:07:29 +0300

    3310: intial input -> adjacency list

--- 3310.py
+++ 3310.py
@@ -6,3 +6,7 @@ class Solution(object):
         :type invocations: List[List[int]]
         :rtype: List[int]
         """
+
+        adjacency_list = [[] for i in range(n)] # for i-th vertex: [v0, v4, ..., v.] <- all vertices adjacent to vi
+        for u, v in invocations:
+            adjacency_list[u].append(v)
