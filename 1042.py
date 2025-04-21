c9f359796b1bba083e353c493f9e34b50711c4ae
Danil Mikhaylov <damikh13@gmail.com>
2025-04-21 23:06:38 +0300

    1042: 0-indexing vertices at the very beginning

--- 1042.py
+++ 1042.py
@@ -5,6 +5,10 @@ class Solution(object):
         :type paths: List[List[int]]
         :rtype: List[int]
         """
+        # make vertices 0-indexed (1,n -> 0,n-1)
+        for i in range(len(paths)):
+            paths[i][0] -= 1
+            paths[i][1] -= 1
 
         adjacency_list = [[] for _ in range(n)]
         for u, v in paths:
