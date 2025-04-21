16da6abbf52089beae9e4667e911792c1bf77e48
Danil Mikhaylov <damikh13@gmail.com>
2025-04-21 23:07:44 +0300

    1042: fill coloring by forbidding colors of adj vertices and choosing from what's left

--- 1042.py
+++ 1042.py
@@ -14,3 +14,21 @@ class Solution(object):
         for u, v in paths:
             adjacency_list[u].append(v)
             adjacency_list[v].append(u)
+
+        coloring = [0] * n # for i-th garden: flower type (1-4), 0 if not assigned
+
+        for curr_vertex in range(n):
+            forbidden_colors = set()
+
+            # forbid colors of adjacent gardens
+            for neighbor in adjacency_list[curr_vertex]:
+                if coloring[neighbor] != 0:
+                    forbidden_colors.add(coloring[neighbor])
+
+            # assign the first available color
+            for color in range(1, 5):
+                if color not in forbidden_colors:
+                    coloring[curr_vertex] = color
+                    break
+
+        return coloring
