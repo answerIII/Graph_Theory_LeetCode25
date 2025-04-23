55fe06027e7698a2819004d09961f242073b20ca
Danil Mikhaylov <damikh13@gmail.com>
2025-04-23 22:43:45 +0300

    1782: renambe vars (u,v -> a,b for consistency), removed comments

--- 1782.py
+++ 1782.py
@@ -1,10 +1,6 @@
 from collections import defaultdict
 
 class Solution(object):
-    # @staticmethod
-    # def incident(a,b, deg, shared):
-    #     return deg[a] + deg[b] - shared[(min(a, b), max(a, b))]
-
     def countPairs(self, n, edges, queries):
         """
         :type n: int
@@ -12,41 +8,41 @@ class Solution(object):
         :type queries: List[int]
         :rtype: List[int]
         """
+        deg = [0] * (n + 1)
+        shared = defaultdict(int)
 
-        deg = defaultdict()
-        for i in range(1, n+1):
-            deg[i] += 0
-        shared = defaultdict(lambda: 0)
+        for a, b in edges:
+            deg[a] += 1
+            deg[b] += 1
+            smaller_vertex = min(a, b)
+            bigger_vertex = max(a, b)
+            edge = (smaller_vertex, bigger_vertex) # so (1,2) and (2,1) are the same
+            shared[edge] += 1
 
-        for u, v in edges:
-            deg[u] += 1
-            deg[v] += 1
-            first_vertex = min(u, v)
-            second_vertex = max(u, v)
-            shared[(first_vertex, second_vertex)] += 1 # so (1, 2) and (2, 1) are the same
+        vertices_by_deg = sorted(range(1, n + 1), key=lambda x: deg[x])
 
-        deg_sorted = sorted(deg.items(), key=lambda x: x[1]) # sort by degree
-
-        queries_len = len(queries)
-        answers = [0] * queries_len
-        for i in range(queries_len):
+        answers = []
+        for query in queries:
             count = 0
             L = 0
             R = n - 1
+
             while L < R:
-                a = deg_sorted[L][0]
-                b = deg_sorted[R][0]
-                if deg[a] + deg[b] > queries[i]:
+                left_vertex = vertices_by_deg[L]
+                right_vertex = vertices_by_deg[R]
+                if deg[left_vertex] + deg[right_vertex] > query:
                     count += (R - L)
                     R -= 1
                 else:
                     L += 1
                 
             # account for shared, too
-            for a, b in shared.keys():
-                a_b_was_counted_previously = deg[a] + deg[b] > queries[i]
-                a_b_with_account_for_shared_is_wrong = deg[a] + deg[b] - shared[(min(a, b), max(a, b))] <= queries[i]
+            for (a, b), shared_count in shared.items():
+                a_b_was_counted_previously = deg[a] + deg[b] > query
+                a_b_with_account_for_shared_is_wrong = deg[a] + deg[b] - shared_count <= query
+                
                 if a_b_was_counted_previously and a_b_with_account_for_shared_is_wrong:
                     count -= 1
-            answers[i] = count
+
+            answers.append(count)
         return answers
