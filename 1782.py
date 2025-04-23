5d43e28bc50c218d5923aa32ba9ad206fe3ed464
Danil Mikhaylov <damikh13@gmail.com>
2025-04-23 21:02:56 +0300

    1782: + deg and shared dicts to later count inc(a,b) = deg(a)+deg(b)-shared(a,b)

--- 1782.py
+++ 1782.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 class Solution(object):
     def countPairs(self, n, edges, queries):
         """
@@ -6,4 +8,13 @@ class Solution(object):
         :type queries: List[int]
         :rtype: List[int]
         """
-        
+
+        deg = defaultdict(lambda: 0)
+        shared = defaultdict(lambda: 0)
+
+        for u, v in edges:
+            deg[u] += 1
+            deg[v] += 1
+            first_vertex = min(u, v)
+            second_vertex = max(u, v)
+            shared[(first_vertex, second_vertex)] += 1 # so (1, 2) and (2, 1) are the same
