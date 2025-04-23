f3eea761b46bc819f361660dc7a86afd3b9b002c
Danil Mikhaylov <damikh13@gmail.com>
2025-04-23 22:16:58 +0300

    1782: O(q(n+e)) solution

--- 1782.py
+++ 1782.py
@@ -1,9 +1,9 @@
 from collections import defaultdict
 
 class Solution(object):
-    @staticmethod
-    def incident(a,b, deg, shared):
-        return deg[a] + deg[b] - shared[(min(a, b), max(a, b))]
+    # @staticmethod
+    # def incident(a,b, deg, shared):
+    #     return deg[a] + deg[b] - shared[(min(a, b), max(a, b))]
 
     def countPairs(self, n, edges, queries):
         """
@@ -13,7 +13,9 @@ class Solution(object):
         :rtype: List[int]
         """
 
-        deg = defaultdict(lambda: 0)
+        deg = defaultdict()
+        for i in range(1, n+1):
+            deg[i] += 0
         shared = defaultdict(lambda: 0)
 
         for u, v in edges:
@@ -23,14 +25,28 @@ class Solution(object):
             second_vertex = max(u, v)
             shared[(first_vertex, second_vertex)] += 1 # so (1, 2) and (2, 1) are the same
 
+        deg_sorted = sorted(deg.items(), key=lambda x: x[1]) # sort by degree
+
         queries_len = len(queries)
         answers = [0] * queries_len
         for i in range(queries_len):
             count = 0
-            for u in range(1, n+1):
-                for v in range(u+1, n+1):
-                    if self.incident(u, v, deg, shared) > queries[i]:
-                        count += 1
+            L = 0
+            R = n - 1
+            while L < R:
+                a = deg_sorted[L][0]
+                b = deg_sorted[R][0]
+                if deg[a] + deg[b] > queries[i]:
+                    count += (R - L)
+                    R -= 1
+                else:
+                    L += 1
+                
+            # account for shared, too
+            for a, b in shared.keys():
+                a_b_was_counted_previously = deg[a] + deg[b] > queries[i]
+                a_b_with_account_for_shared_is_wrong = deg[a] + deg[b] - shared[(min(a, b), max(a, b))] <= queries[i]
+                if a_b_was_counted_previously and a_b_with_account_for_shared_is_wrong:
+                    count -= 1
             answers[i] = count
-
         return answers
