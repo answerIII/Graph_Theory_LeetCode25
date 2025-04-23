6159ad64df001567a01152cb0ef10cabf7ffc47e
chernetsov mihail <mishavvvsweater@gmail.com>
2025-04-23 12:46:22 +0300

    good idea but should use prior que instead of stack

--- task_1514/src/main/java/org/example/Main.java
+++ task_1514/src/main/java/org/example/Main.java
@@ -8,49 +8,27 @@ import java.util.Stack;
 import java.util.function.ToIntFunction;
 
 class Solution {
-  class EdgeWithProb {
-    int[] edge;
-    double prob;
-
-    public EdgeWithProb(int[] edge, double prob) {
-      this.edge = edge;
-      this.prob = prob;
-    }
-  }
   public double maxProbability(int n, int[][] edges, double[] succProb, int start_node, int end_node) {
-    EdgeWithProb[] combined = new EdgeWithProb[edges.length];
-    for (int i = 0; i < edges.length; i++) {
-      combined[i] = new EdgeWithProb(edges[i], succProb[i]);
+    List<List<double[]>> graph = new ArrayList<>();
+    for (int i = 0; i < n; i++) {
+      graph.add(new ArrayList<>());
     }
-    Arrays.sort(combined, Comparator.comparingInt(edgeWithProb -> edgeWithProb.edge[0])); // e log e
-
+    for (int i = 0; i < edges.length; i++) {
+      int a = edges[i][0];
+      int b = edges[i][1];
+      double prob = succProb[i];
+      graph.get(a).add(new double[]{b, prob});
+      graph.get(b).add(new double[]{a, prob});
+    }
     Stack<Integer> stack = new Stack<>();
+    int[] probabilities = new int[n];
+    probabilities[start_node] = 1;
     stack.push(start_node);
     while(!stack.empty()){
-      int node = stack.pop();
-      int index = binarySearch(combined, node);
-    }
-    return 0;
-  }
-
-  public static int binarySearch(EdgeWithProb[] arr, int target) {
-    int left = 0;
-    int right = arr.length - 1;
-    while (left <= right) {
-      int mid = left + (right - left) / 2;
-      if (arr[mid].edge[0] == target) {
-        return mid;
-      } else if (arr[mid].edge[0] < target) {
-        left = mid + 1;
-      } else {
-        right = mid - 1;
+      int parent = stack.pop();
+      for(double[] friends : graph.get(parent)){
+        probabilities[(int)friends[0]]
       }
-    }
-    return -1;
-  }
-  void findMinPath(List<List<Integer>> lists, int start_node, int end_node, double[] succProb, int[] pathLengths){
-    for(Integer indexNode : lists.get(start_node)){
-      if(pathLengths[indexNode] > pathLengths[start_node])
     }
   }
 }
