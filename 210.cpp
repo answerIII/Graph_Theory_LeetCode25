7e87b4f771f31faf2d9d6493c4c05724fab5ef6d
IvanuilC <160422575+IvanuilC@users.noreply.github.com>
2025-04-24 01:00:14 +0300

    210.cpp

--- 210.cpp
+++ 210.cpp
@@ -3,16 +3,15 @@
 class Solution {
 public:
     int current_label;
-    int now;
     char* visited;
     std::vector<int> res;
-    void dfs(std::vector<std::vector<int>>& prerequisites, int v) {
+    void dfs(std::vector<std::vector<int>>& adj, int v) {
         visited[v] = 1;
-        for (auto p : prerequisites) {
-            if (p[1] == v && visited[p[0]] == 0) {
-                dfs(prerequisites, p[0]);
+        for (auto p : adj[v]) {
+            if (visited[p] == 0) {
+                dfs(adj, p);
             }
-            else if (p[1] == v && visited[p[0]] == 1) {
+            else if (visited[p] == 1) {
                 current_label = -2;
                 return;
             }
@@ -25,17 +24,19 @@ public:
         --current_label;
     }
     std::vector<int> findOrder(int numCourses, std::vector<std::vector<int>>& prerequisites) {
+        std::vector<std::vector<int>> adj(numCourses);
+        for (const auto& edge : prerequisites) {
+            adj[edge[1]].push_back(edge[0]);
+        }
         visited = new char[numCourses];
         current_label = numCourses - 1;
         res.resize(numCourses);
         for (int i = 0; i < numCourses; ++i) {
             visited[i] = 0;
-            res[i] = -1;
         }
         for (int i = 0; i < numCourses; ++i) {
-            if (!visited[i]) {
-                now = i;
-                dfs(prerequisites, i);
+            if (visited[i] == 0) {
+                dfs(adj, i);
                 if (current_label == -2) {
                     break;
                 }
