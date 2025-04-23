8c068b0a6159f4646a478bc6d6814dd7ed0de6eb
Danil Mikhaylov <damikh13@gmail.com>
2025-04-23 21:17:08 +0300

    1782: n^2 solution: check incidents for every pair(a,b) and count the ones < query

--- 1782.py
+++ 1782.py
@@ -1,6 +1,10 @@
 from collections import defaultdict
 
 class Solution(object):
+    @staticmethod
+    def incident(a,b, deg, shared):
+        return deg[a] + deg[b] - shared[(min(a, b), max(a, b))]
+
     def countPairs(self, n, edges, queries):
         """
         :type n: int
@@ -18,3 +22,15 @@ class Solution(object):
             first_vertex = min(u, v)
             second_vertex = max(u, v)
             shared[(first_vertex, second_vertex)] += 1 # so (1, 2) and (2, 1) are the same
+
+        queries_len = len(queries)
+        answers = [0] * queries_len
+        for i in range(queries_len):
+            count = 0
+            for u in range(1, n+1):
+                for v in range(u+1, n+1):
+                    if self.incident(u, v, deg, shared) > queries[i]:
+                        count += 1
+            answers[i] = count
+
+        return answers
