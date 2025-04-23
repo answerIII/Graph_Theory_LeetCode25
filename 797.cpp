ed1908c0c1a30458a1e7fee26f56cc3e29c62efd
IvanuilC <160422575+IvanuilC@users.noreply.github.com>
2025-04-24 01:54:32 +0300

    797.cpp
    One way

--- 797.cpp
+++ 797.cpp
@@ -3,24 +3,24 @@
 class Solution {
 public:
     std::vector<std::vector<int>> res;
+    std::vector<int> way;
     int n;
-    void dfs(std::vector<std::vector<int>>& graph, int v, std::vector<int> way) {
+    void dfs(std::vector<std::vector<int>>& graph, int v) {
         for (auto p : graph[v]) {
-            std::vector<int> nway = way;
-            nway.push_back(p);
+            way.push_back(p);
             if (p == n - 1) {
-                res.push_back(nway);
+                res.push_back(way);
             }
             else {
-                dfs(graph, p, nway);
+                dfs(graph, p);
             }
+            way.pop_back();
         }
     }
     std::vector<std::vector<int>> allPathsSourceTarget(std::vector<std::vector<int>>& graph) {
         n = graph.size();
-        std::vector<int> way;
         way.push_back(0);
-        dfs(graph, 0, way);
+        dfs(graph, 0);
         return res;
     }
 };
