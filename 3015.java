fa7aa438ad9c0ee8c726f7aa07b464cb41d65166
Me <mk>
2025-04-24 23:48:10 +0300

    update solution for task 3015, replaced the postfix increment with a prefix increment
    Passed 880/880 test cases. Runtime: 2 ms (beats 100.00% of solutions by runtime), Memory: 44.47 MB (beats 86.59% of solutions by memory).

--- 3015.java
+++ 3015.java
@@ -2,8 +2,8 @@ class Solution {
     public int[] countOfPairs(int n, int x, int y) {
         int[] res = new int[n];
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = i + 1; j <= n; j++) {
+        for (int i = 1; i <= n; ++i) {
+            for (int j = i + 1; j <= n; ++j) {
                 int direct = j - i;
                 int viaXY = Math.abs(i - x) + 1 + Math.abs(j - y);
                 int viaYX = Math.abs(i - y) + 1 + Math.abs(j - x);
