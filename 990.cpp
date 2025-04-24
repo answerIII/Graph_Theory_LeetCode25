b84471a162a92364337320bcc8ab3fc2346c0e41
Denis <usikdenisik@yandex.ru>
2025-04-24 23:12:12 +0300

    990 attempt 1 (WA)

---
+++ 990.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+
+    static constexpr int ALPH_SIZE = 26;
+
+    int values[ALPH_SIZE]{};
+    bool vars[ALPH_SIZE]{ false };
+    vector<pair<int, bool>> g[ALPH_SIZE];
+    int max_value = -1;
+
+    bool dfs(const int v, const int prev, bool equal) {
+        if (values[v] != -1) {
+            return !(equal && values[v] != values[prev] || !equal && values[v] == values[prev]);
+        }
+        if (v == prev) { // значит только начали обход
+            max_value = -1;
+        }
+        if (equal) {
+            values[v] = values[prev];
+        } else {
+            values[v] = ++max_value;
+        }
+        return std::ranges::all_of(g[v], [v, this](const auto& u) {
+            return dfs(u.first, v, u.second);
+        });
+    }
+    bool equationsPossible(vector<string>& equations) {
+        for (auto& x : values) {
+            x = -1;
+        }
+        for (const auto& s : equations) {
+            g[s[0] - 'a'].emplace_back(s[3] - 'a', s[1] == '=');
+            vars[s[0] - 'a'] = true;
+            vars[s[3] - 'a'] = true;
+        }
+        for (int v = 0; v < ALPH_SIZE; ++v) {
+            if (vars[0] && values[v] == -1 && !dfs(v, v, false)) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
