807f14ca3590fe7bb2e45e979ec4dbfb160aca56
Me <mk>
2025-04-24 23:48:00 +0300

    add solution for task 3015
    Passed 880/880 test cases. Runtime: 3 ms (beats 88.27% of solutions by runtime), Memory: 44.40 MB (beats 91.06% of solutions by memory).

---
+++ 3015.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] countOfPairs(int n, int x, int y) {
+        int[] res = new int[n];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = i + 1; j <= n; j++) {
+                int direct = j - i;
+                int viaXY = Math.abs(i - x) + 1 + Math.abs(j - y);
+                int viaYX = Math.abs(i - y) + 1 + Math.abs(j - x);
+                int d = Math.min(direct, Math.min(viaXY, viaYX));
+
+                res[d - 1] += 2;
+            }
+        }
+        return res;
+    }
+}
