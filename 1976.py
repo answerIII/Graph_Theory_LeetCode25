f673be808bf6ebd6bfecf61bee6c26448d0b1fdd
Pletka Daniil <90145262+PletkaDaniil@users.noreply.github.com>
2025-04-26 19:40:01 +0300

    finally 1976 with runtime 70.14% and memory 97.46%

--- 1976.py
+++ 1976.py
@@ -3,14 +3,10 @@ import heapq
 class Solution(object):
     def countPaths(self, n, roads):
 
-        graph = {}
+        graph = {i: {} for i in range(n)}
         for u, v, time in roads:
-            if u not in graph:
-                graph[u] = []
-            if v not in graph:
-                graph[v] = []
-            graph[u].append((v, time))
-            graph[v].append((u, time))
+            graph[u][v] = time
+            graph[v][u] = time
         
         weights = [float('inf')] * n
         weights[0] = 0
@@ -19,20 +15,21 @@ class Solution(object):
         ways[0] = 1
 
         heap = [(0, 0)]
+
         while heap:
             weight, min_node = heapq.heappop(heap)
 
             if weight > weights[min_node]:
                 continue
 
-            for neighbor, time in graph.get(min_node, []):
+            for neighbor, time in graph[min_node].items():
 
                 if weights[neighbor] > weights[min_node] + time:
                     weights[neighbor] = weights[min_node] + time
                     ways[neighbor] = ways[min_node]
-                    heapq.heappush(heap, (weights[neighbor], neighbor))
+                    heapq.heappush(heap, (weights[min_node] + time, neighbor))
 
-                elif weights[neighbor] == weights[min_node] + time:
-                    ways[neighbor] = (ways[neighbor] + ways[min_node]) % (10**9 + 7)
+                elif weights[min_node] + time == weights[neighbor]:
+                    ways[neighbor] = (ways[neighbor] + ways[min_node])
 
-        return ways[n - 1]
+        return ways[n - 1] % (10**9 + 7)
