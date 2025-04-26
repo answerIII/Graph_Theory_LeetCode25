719c0a66abb0f56875d7148c9608df0317fc0248
chernetsov mihail <mishavvvsweater@gmail.com>
2025-04-26 15:34:44 +0300

    second attempt - work but sloooow////////. ((((((((((((((

--- task_1557/src/main/java/org/example/Main.java
+++ task_1557/src/main/java/org/example/Main.java
@@ -13,21 +13,29 @@ class Solution {
       this.number = number;
     }
 
-    public void findParent(List<Integer> vertexes) {
-      if(parents.size() == 0 && isFound){
-        vertexes.set(number,-1);
+    public void findParent(List<Integer> vertexes, boolean byCycle) {
+      if(isFound && parents.size() == 0 && !byCycle) {
+        vertexes.set(number, -1);
+        return;
       }
-      if(isFound) return;
+      else if(parents.size() == 0){
+        vertexes.set(number, number);
+        return;
+      }
+      if (isFound) return;
       isFound = true;
       for(Node parent: parents){
-        parent.findParent(vertexes);
+        parent.findParent(vertexes, false);
       }
     }
 
   }
   public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> edges) {
     Node[] nodes = new Node[n];
-    List<Integer> vertexes = new ArrayList<>();
+    List<Integer> vertexes = new ArrayList<>(n);
+    for (int i=0; i< n ; ++i) {
+      vertexes.add(-1);
+    }
     for (int i=0; i < n; ++i) {
       nodes[i] = new Node(i);
     }
@@ -35,7 +43,7 @@ class Solution {
       nodes[edge.get(1)].parents.add(nodes[edge.get(0)]);
     }
     for (Node node : nodes) {
-      if (!node.isFound) node.findParent(vertexes);
+      if (!node.isFound) node.findParent(vertexes, true);
     }
     List<Integer> minVertexes = new ArrayList<>();
     for (Integer vertex : vertexes) {
@@ -48,7 +56,16 @@ class Solution {
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Solution s=new Solution();
+    int n = 6;
+    List<List<Integer>> edges = new ArrayList<>();
+    edges.add(Arrays.asList(0, 1));
+    edges.add(Arrays.asList(0, 2));
+    edges.add(Arrays.asList(2, 5));
+    edges.add(Arrays.asList(3, 4));
+    edges.add(Arrays.asList(4, 2));
+
+    System.out.println(s.findSmallestSetOfVertices(n,edges));
   }
 
 }
