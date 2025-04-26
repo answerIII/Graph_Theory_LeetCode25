a09bfce9f8d83c7f96c4323580761fda2752884f
chernetsov mihail <mishavvvsweater@gmail.com>
2025-04-26 14:22:22 +0300

    make optimization

--- task_2976/src/main/java/org/example/Main.java
+++ task_2976/src/main/java/org/example/Main.java
@@ -19,41 +19,41 @@ class Solution {
       paths.get((int)original[i] - OFFSET)
               .add(new int[] {(int)changed[i] - OFFSET, cost[i]});
     }
+    pullMostCheapest(paths,vocabulary);
     for(int i = 0; i < source.length(); ++i){
-      int sourceS = (int) source.charAt(i)- OFFSET;
-      int targetS = (int) target.charAt(i)- OFFSET;
-      if(vocabulary[sourceS][targetS] == 0 && source.charAt(i)!=target.charAt(i)) {
-        vocabulary[sourceS][targetS] = findMostCheapest(paths,sourceS, targetS);
+      int sourceS = (int) source.charAt(i) - OFFSET;
+      int targetS = (int) target.charAt(i) - OFFSET;
+      if(source.charAt(i)!=target.charAt(i) && vocabulary[sourceS][targetS] == -1) {
+        return -1;
       }
-      if (vocabulary[sourceS][targetS] == -1) return -1;
       totalCost+=vocabulary[sourceS][targetS];
     }
     return totalCost;
   }
 
-  private int findMostCheapest(List<List<int[]>> paths, int sourceS, int targetS) {
-    int[] probabilities = new int[28];
-    Arrays.fill(probabilities,Integer.MAX_VALUE);
-    probabilities[sourceS] = 0;
-    PriorityQueue<int[]> queue = new PriorityQueue<>(Comparator.comparingDouble(a -> a[1]));
-    queue.add(new int[]{sourceS, 0});
-
-
-    while(!queue.isEmpty()){
-      int[] parent = queue.poll();
-      if(parent[0] == targetS) {
-        return parent[1];
-      }
-      for(int[] friend : paths.get(parent[0])){
-        int friendIndex = friend[0];
-        int way = friend[1] + parent[1];
-        if(way < probabilities[friendIndex]){
-          probabilities[friendIndex] = way;
-          queue.add(new int[]{friendIndex, way});
+  private void pullMostCheapest(List<List<int[]>> paths, int[][] vocabulary) {
+    for(int i=0; i < paths.size(); ++i){
+      Arrays.fill(vocabulary[i], Integer.MAX_VALUE);
+      vocabulary[i][i] = 0;
+    }
+    for(int i=0; i < 26; ++i){
+      PriorityQueue<int[]> queue = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
+      queue.add(new int[]{i, 0});
+      while(!queue.isEmpty()){
+        int[] parent = queue.poll();
+        if(parent[1] > vocabulary[i][parent[0]]) {
+          continue;
+        }
+        for(int[] friend : paths.get(parent[0])){
+          int friendIndex = friend[0];
+          int way = friend[1] + parent[1];
+          if(way < vocabulary[i][friendIndex]){
+            vocabulary[i][friendIndex] = way;
+            queue.add(new int[]{friendIndex, way});
+          }
         }
       }
     }
-    return -1;
   }
 
 }
