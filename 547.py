429d8dc5b7274874976695999785d9f81f82f834
rozotto <dimaegorov915@gmail.com>
2025-04-28 00:53:27 +0700

    3rd try of 547

--- 547.py
+++ 547.py
@@ -7,11 +7,17 @@ class Solution:
         answer = []
         for i in isConnected:
             answer.append(i.count(1))
-        answer = len(list(set(answer)))
-
-        return answer
+        print(answer)
+        if len(list(set(answer))) == 1:
+            return len(answer)
+        else:
+            return len(list(set(answer)))
 
 
 sol = Solution()
 isConnected = [[1, 1, 0], [1, 1, 0], [0, 0, 1]]
+print(sol.findCircleNum(isConnected))
+
+sol = Solution()
+isConnected = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
 print(sol.findCircleNum(isConnected))
