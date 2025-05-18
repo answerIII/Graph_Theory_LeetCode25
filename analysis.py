1a7e35cfd4fcbc15dbb0837d87f4f8991fc6ec6a
Maria Mikhailova <mariiemik@gmail.com>
2025-05-18 15:34:48 +0300

    fix output

--- analysis.py
+++ analysis.py
@@ -6,17 +6,20 @@ import networkx as nx
 def print_analysis(graph: dict, directed: bool, graph_with_correct_result: nx.Graph):
     num_of_vertices = number_of_vertices(graph)
     num_of_edges = number_of_edges(graph, directed)
-    print("кол-во вершин = " + num_of_vertices + " (должно быть " + graph_with_correct_result.number_of_nodes + ")")
-    print("кол-во вершин = " + num_of_edges + " (должно быть " + graph_with_correct_result.number_of_edges + ")")
-
-    print(density(num_of_edges, num_of_vertices) + " (должно быть " + nx.density(graph_with_correct_result)+ ")")
+    print(f"кол-во вершин = {num_of_vertices} (должно быть {graph_with_correct_result.number_of_nodes()})")
+    print(f"кол-во вершин =  {num_of_edges} (должно быть {graph_with_correct_result.number_of_edges()})")
+    print(f"плотность = {density(num_of_edges, num_of_vertices)} (должно быть {nx.density(graph_with_correct_result)})")
     week_max_component : set  # самая большая компонента по количеству вершин
     week_component_count: int
     if directed:
         week_component_count, week_max_component = weekly_connected_components(to_undirected(graph))
+        correct_components = len(list(nx.weakly_connected_components(graph_with_correct_result)))
+
     else:
         week_component_count, week_max_component = weekly_connected_components(graph)
-    
-    print(week_component_count + " (должно быть " + nx.weakly_connected_components(graph_with_correct_result)+ ")")
+        correct_components = len(list(nx.connected_components(graph_with_correct_result)))
 
+    
+    print(f"кол-во комп. слабой свзяности = {week_component_count} (должно быть {correct_components})")
+    print(f"дол в вершин в макс. по мощности компоненте = {len(week_max_component)/num_of_vertices}")
     
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from basic import load_graph_from_file
 from analysis import *
 def main():
-    directed: bool = False
-    G, graph_with_correct_result = load_graph_from_file("data/undirected/CA-AstroPh.txt", directed)
+    directed: bool = True
+    G, graph_with_correct_result = load_graph_from_file("data/example_graph.txt", directed)
     print_analysis(G, directed, graph_with_correct_result)
 
 
