73c720b67073aaf62104dc493d4befa48551d80b
Isabel Panova <panova.isabel@gmail.com>
2025-05-20 14:00:57 +0300

    output method updated

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "classes/Graph.h"
 #include "classes/Parser.h"
 #include "functions/parserf.h"
-
+#include "functions/CsvParser.h"
 int main(const int argc, const char *argv[]) {
     validateArgs(argc, argv);
     bool directed = (std::string(argv[2]) == "directed");
@@ -21,7 +21,7 @@ int main(const int argc, const char *argv[]) {
         UndirectedGraph undirectedGraph(*graph);
         std::cout << undirectedGraph.getAverageClusteringCoefficient();
     }
-
+    //degree_distribution(argv[1], argv[2], directedGraph);
     delete graph;
     return 0;
 }
