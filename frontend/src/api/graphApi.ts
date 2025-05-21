6efa9c67d9ee3a4507370d5e7b62188177432257
LeshiyAda9H <114980114+LeshiyAda9H@users.noreply.github.com>
2025-05-21 20:48:04 +0300

    feat(frontend): add post api-url/api/dataset

--- frontend/src/api/graphApi.ts
+++ frontend/src/api/graphApi.ts
@@ -6,12 +6,25 @@ const API_URL = 'http://localhost:8085';
 export const uploadGraph = async (graph: Graph): Promise<void> => {
 
     try{
-        console.log(graph);
+        // console.log(graph);
         await axios.post(`${API_URL}/upload`, graph, {
             headers: {'Content-Type' : 'application/json'},
         });
     }
     catch(error){
         throw new Error('Ошибка отправки графа на бэкенд' + (error as Error).message);
+    }
+}
+
+export const selectDataset = async (datasetName: string): Promise<void> => {
+
+    try{
+        
+        await axios.post(`${API_URL}/api/dataset`, datasetName, {
+            headers: {'Content-Type' : 'application/json'},
+        });
+    }
+    catch (error){
+        throw new Error(`Ошибка выбора датасета ${datasetName}` + (error as Error).message);
     }
 }
