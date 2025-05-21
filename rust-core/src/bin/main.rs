f5c4d8791ac24596aa00c2f6157fd9a5ad1ac6d5
dav1dvorobev <dav1dvorobev@vk.com>
2025-05-21 14:34:19 +0300

    chore(rust-core): update main.rs

--- rust-core/src/bin/main.rs
+++ rust-core/src/bin/main.rs
@@ -1,1 +1,7 @@
-fn main() {}
+use rust_core::Graph;
+
+fn main() -> rust_core::Result<()> {
+    let graph = Graph::from_msgpack("../.storage/processed_CA-GrQc.msgpack")?;
+    println!("{:?}", graph);
+    Ok(())
+}
