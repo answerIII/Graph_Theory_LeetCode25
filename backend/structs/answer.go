24c73ef4f3750435e37f866cb3248e95e8307c75
hikkmind <liferisinghigh@yandex.ru>
2025-05-24 16:12:10 +0300

    chore(backend): add answer1 tags

--- backend/structs/answer.go
+++ backend/structs/answer.go
@@ -1,14 +1,15 @@
 package structs
 
 type AnswerA1 struct {
-	WCC           Graph
-	VertexCount   int
-	EdgesCount    int
-	Density       float64
-	ProportionWCC float32
-	WCCCount      int
-	ProportionSCC float32
-	SCCCount      int
+	WCC           Graph   `json:"-"`
+	Directed      bool    `json:"directed"`
+	VertexCount   int     `json:"vertexCount"`
+	EdgesCount    int     `json:"edgesCount"`
+	Density       float64 `json:"density"`
+	ProportionWCC float32 `json:"propMaxWeakComponent"`
+	WCCCount      int     `json:"weakComponentCount"`
+	ProportionSCC float32 `json:"propMaxStrongComponent"`
+	SCCCount      int     `json:"strongComponentCount"`
 }
 
 type AnswerA2 struct {
