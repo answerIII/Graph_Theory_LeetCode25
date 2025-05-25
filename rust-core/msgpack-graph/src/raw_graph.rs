a79a4d9366cb3487dd1cf03511a3701541ffae30
dav1dvorobev <dav1dvorobev@vk.com>
2025-05-25 11:31:45 +0300

    chore(rust-core): update raw_graph.rs; update graph.rs

--- rust-core/msgpack-graph/src/raw_graph.rs
+++ rust-core/msgpack-graph/src/raw_graph.rs
@@ -1,13 +1,27 @@
 use crate::RMPSupport;
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize)]
 pub struct RawGraph {
     #[serde(rename = "nodeCount")]
-    pub(crate) node_count: usize,
+    node_count: usize,
     #[serde(rename = "edgeCount")]
-    pub(crate) edge_count: usize,
-    pub(crate) edges: Vec<[usize; 2]>,
+    edge_count: usize,
+    edges: Vec<[usize; 2]>,
 }
 
 impl RMPSupport for RawGraph {}
+
+impl RawGraph {
+    pub fn node_count(&self) -> usize {
+        self.node_count
+    }
+
+    pub fn edge_count(&self) -> usize {
+        self.edge_count
+    }
+
+    pub fn edges(&self) -> &Vec<[usize; 2]> {
+        &self.edges
+    }
+}
