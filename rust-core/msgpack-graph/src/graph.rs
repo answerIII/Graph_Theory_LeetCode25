8b48a66b93b66447cffa250649e76de1f8544293
dav1dvorobev <dav1dvorobev@vk.com>
2025-05-26 22:06:44 +0300

    feat(rust-core): add fn distance(); raname distances() -> sh ortest_paths(); add BFSNodeState

--- rust-core/msgpack-graph/src/graph.rs
+++ rust-core/msgpack-graph/src/graph.rs
@@ -26,9 +26,15 @@ impl From<RawGraph> for Graph {
     }
 }
 
+#[derive(Serialize, Deserialize)]
+pub struct BFSNodeState {
+    pub distance: usize,
+    pub previous_node: Option<usize>,
+}
+
 impl Graph {
-    /// Breadth-First Search: finds the shortest paths from **`start`** to **all other reachable**
-    pub fn distances(&self, start: usize) -> HashMap<usize, usize> {
+    /// Breadth-First Search: finds the shortest distance from **`start`** to **`end`**
+    pub fn distance(&self, start: usize, end: usize) -> Option<usize> {
         let mut distances = HashMap::with_capacity(self.node_count);
         let mut queue = VecDeque::with_capacity(self.node_count);
         distances.insert(start, 0);
@@ -38,6 +44,40 @@ impl Graph {
                 for &neighbour in neighbours {
                     if !distances.contains_key(&neighbour) {
                         distances.insert(neighbour, distances[&node] + 1);
+                        if neighbour == end {
+                            return distances.get(&neighbour).copied();
+                        }
+                        queue.push_back(neighbour);
+                    }
+                }
+            }
+        }
+        None
+    }
+
+    /// Breadth-First Search: finds the shortest paths from **`start`** to **all other reachable**
+    pub fn shortest_paths(&self, start: usize) -> HashMap<usize, BFSNodeState> {
+        let mut distances = HashMap::with_capacity(self.node_count);
+        let mut queue = VecDeque::with_capacity(self.node_count);
+        distances.insert(
+            start,
+            BFSNodeState {
+                distance: 0,
+                previous_node: None,
+            },
+        );
+        queue.push_back(start);
+        while let Some(node) = queue.pop_front() {
+            if let Some(neighbours) = self.adjacency_list.get(&node) {
+                for &neighbour in neighbours {
+                    if !distances.contains_key(&neighbour) {
+                        distances.insert(
+                            neighbour,
+                            BFSNodeState {
+                                distance: distances[&node].distance + 1,
+                                previous_node: Some(node),
+                            },
+                        );
                         queue.push_back(neighbour);
                     }
                 }
