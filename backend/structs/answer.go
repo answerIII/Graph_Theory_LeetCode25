0a9ee57e2222be30b980a8dad462ca0c7cad5140
hikkmind <liferisinghigh@yandex.ru>
2025-05-27 20:36:24 +0300

    chore(backend): update answer tags

--- backend/structs/answer.go
+++ backend/structs/answer.go
@@ -3,13 +3,13 @@ package structs
 type AnswerA1 struct {
 	WCC           *Graph  `json:"-" msgpack:"-"`
 	Directed      bool    `json:"directed" msgpack:"directed"`
-	VertexCount   int     `json:"vertexCount" msgpack:"vertexCount"`
+	VertexCount   int     `json:"nodeCount" msgpack:"nodeCount"`
 	EdgesCount    int     `json:"edgesCount" msgpack:"edgesCount"`
 	Density       float64 `json:"density" msgpack:"density"`
-	ProportionWCC float32 `json:"propMaxWeakComponent" msgpack:"propMaxWeakComponent"`
-	WCCCount      int     `json:"weakComponentCount" msgpack:"weakComponentCount"`
-	ProportionSCC float32 `json:"propMaxStrongComponent" msgpack:"propMaxStrongComponent"`
-	SCCCount      int     `json:"strongComponentCount" msgpack:"strongComponentCount"`
+	WCCCount      int     `json:"wccCount" msgpack:"wccCount"`
+	ProportionWCC float32 `json:"proportionWCC" msgpack:"proportionWCC"`
+	SCCCount      int     `json:"sccCount" msgpack:"sccCount"`
+	ProportionSCC float32 `json:"proportionSCC" msgpack:"proportionSCC"`
 }
 
 type AnswerA2 struct {
