e0d916ec33a42faeded7f99ec15dedde61522c92
dav1dvorobev <dav1dvorobev@vk.com>
2025-05-27 09:09:26 +0300

    feat(rust-core): transfer storage of landmarks in the graph

--- rust-core/msgpack-graph/src/graph.rs
+++ rust-core/msgpack-graph/src/graph.rs
@@ -1,11 +1,35 @@
 use crate::{RMPSupport, RawGraph};
+use rand::{rng, seq::SliceRandom};
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
 use serde::{Deserialize, Serialize};
 use std::collections::{HashMap, VecDeque};
+
+#[derive(Serialize, Deserialize)]
+pub struct Landmark {
+    landmark: usize,
+    distances: HashMap<usize, BFSNodeState>,
+}
+
+impl Landmark {
+    pub fn path_to(&self, mut node: usize) -> Vec<usize> {
+        let mut path = vec![node];
+        while let Some(previous_node) = self
+            .distances
+            .get(&node)
+            .and_then(|state| state.previous_node)
+        {
+            path.push(previous_node);
+            node = previous_node;
+        }
+        path
+    }
+}
 
 #[derive(Serialize, Deserialize)]
 pub struct Graph {
     node_count: usize,
     edge_count: usize,
+    landmarks: Option<Vec<Landmark>>,
     adjacency_list: HashMap<usize, Vec<usize>>,
 }
 
@@ -21,12 +45,35 @@ impl From<RawGraph> for Graph {
         Self {
             node_count: raw_graph.node_count(),
             edge_count: raw_graph.edge_count(),
+            landmarks: None,
             adjacency_list,
         }
     }
 }
 
+pub enum Selection {
+    Random,
+    Degree,
+    Coverage,
+}
+
 impl Graph {
+    pub fn create_landmarks(&mut self, number_of_landmarks: usize, selection: Selection) {
+        self.landmarks = Some(
+            match selection {
+                Selection::Random => self.select_random_landmarks(number_of_landmarks),
+                Selection::Degree => self.select_high_degree_landmarks(number_of_landmarks),
+                Selection::Coverage => self.select_best_coverage_landmarks(number_of_landmarks),
+            }
+            .into_par_iter()
+            .map(|landmark| Landmark {
+                landmark,
+                distances: self.shortest_paths(landmark),
+            })
+            .collect(),
+        );
+    }
+
     /// Breadth-First Search: finds the shortest distance from **`start`** to **`end`**
     pub fn distance(&self, start: usize, end: usize) -> Option<usize> {
         let mut distances = HashMap::with_capacity(self.node_count);
@@ -47,6 +94,33 @@ impl Graph {
             }
         }
         None
+    }
+
+    /// Landmarks-Basic: distance estimation through landmarks
+    pub fn estimate_distance(&self, start: usize, end: usize) -> Option<usize> {
+        let Some(landmarks) = self.landmarks.as_ref() else {
+            panic!("Missing landmarks");
+        };
+        let mut distance: Option<usize> = None;
+        for landmark in landmarks.iter() {
+            if let (Some(start), Some(end)) =
+                (landmark.distances.get(&start), landmark.distances.get(&end))
+            {
+                distance = Some(match distance {
+                    Some(distance) => distance.min(start.distance + end.distance),
+                    None => start.distance + end.distance,
+                });
+            }
+        }
+        distance
+    }
+
+    /// Landmarks-BFS: distance estimation through landmarks from **`start`** to **`end`**
+    pub fn estimate_distance_bfs(&self, start: usize, end: usize) {
+        let Some(landmarks) = self.landmarks.as_ref() else {
+            panic!("Missing landmarks");
+        };
+        todo!()
     }
 
     pub fn node_count(&self) -> usize {
@@ -70,14 +144,14 @@ impl Graph {
 }
 
 #[derive(Serialize, Deserialize)]
-pub(crate) struct BFSNodeState {
-    pub(crate) distance: usize,
-    pub(crate) previous_node: Option<usize>,
+struct BFSNodeState {
+    distance: usize,
+    previous_node: Option<usize>,
 }
 
 impl Graph {
     /// Breadth-First Search: finds the shortest paths from **`start`** to **all other reachable**
-    pub(crate) fn shortest_paths(&self, start: usize) -> HashMap<usize, BFSNodeState> {
+    fn shortest_paths(&self, start: usize) -> HashMap<usize, BFSNodeState> {
         let mut distances = HashMap::with_capacity(self.node_count);
         let mut queue = VecDeque::with_capacity(self.node_count);
         distances.insert(
@@ -105,5 +179,25 @@ impl Graph {
             }
         }
         distances
+    }
+
+    fn select_random_landmarks(&self, number_of_landmarks: usize) -> Vec<usize> {
+        let mut nodes: Vec<usize> = self.adjacency_list().keys().copied().collect();
+        nodes.shuffle(&mut rng());
+        nodes.into_iter().take(number_of_landmarks).collect()
+    }
+
+    fn select_high_degree_landmarks(&self, number_of_landmarks: usize) -> Vec<usize> {
+        let mut degrees = self.degrees();
+        degrees.sort_by(|a, b| b.1.cmp(&a.1));
+        degrees
+            .into_iter()
+            .take(number_of_landmarks)
+            .map(|(node, _)| node)
+            .collect()
+    }
+
+    fn select_best_coverage_landmarks(&self, number_of_landmarks: usize) -> Vec<usize> {
+        todo!()
     }
 }
--- rust-core/msgpack-graph/src/lib.rs
+++ rust-core/msgpack-graph/src/lib.rs
@@ -1,11 +1,9 @@
 mod error;
 mod graph;
-mod landmarks;
 mod raw_graph;
 mod rmp_support;
 
 pub use error::{Error, Result};
-pub use graph::Graph;
-pub use landmarks::{Landmarks, Selection};
+pub use graph::{Graph, Selection};
 pub use raw_graph::RawGraph;
 pub use rmp_support::RMPSupport;
