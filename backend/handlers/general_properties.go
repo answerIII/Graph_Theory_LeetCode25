38696cd54e3473f9ba6c4b9eab324ed047c29440
hikkmind <liferisinghigh@yandex.ru>
2025-05-27 20:54:06 +0300

    feat(backend): add mux to server

--- backend/handlers/general_properties.go
+++ backend/handlers/general_properties.go
@@ -5,10 +5,16 @@ import (
 	"net/http"
 
 	"github.com/HikkMind/graph/middleware"
+	"github.com/gorilla/mux"
 )
 
 func GetGeneralProperties(w http.ResponseWriter, r *http.Request) {
+	storageRoot := "./"
+
+	vars := mux.Vars(r)
+	filename := vars["datasetname"] + ".msgpack"
+
 	w.WriteHeader(http.StatusOK)
-	w.Write(middleware.GenerateA1(middleware.ReadGraph("E:\\Programming\\uni\\graphs\\project\\Graph_Theory_LeetCode25\\.storage\\test.json")))
+	w.Write(middleware.GenerateA1(middleware.ReadGraph(storageRoot + filename)))
 	// fmt.Fprintln(w, string(middleware.GenerateA1(middleware.ReadGraph("E:\\Programming\\uni\\graphs\\project\\Graph_Theory_LeetCode25\\.storage\\test.json"))))
 }
--- backend/main.go
+++ backend/main.go
@@ -10,17 +10,19 @@ import (
 
 	// "github.com/HikkMind/graph/algo"
 	"github.com/HikkMind/graph/handlers"
+	"github.com/gorilla/mux"
 	// "github.com/HikkMind/graph/middleware"
 	// "github.com/vmihailenco/msgpack/v5"
 )
 
 func main() {
+	router := mux.NewRouter()
 
-	http.HandleFunc("/properties", handlers.GetGeneralProperties)
+	// Регистрируем маршрут
+	router.HandleFunc("/graphs/{datasetname}/properties", handlers.GetGeneralProperties).Methods("GET")
 
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":8080", router)
 	if err != nil {
 		panic(err)
 	}
-
 }
