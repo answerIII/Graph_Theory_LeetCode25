e2c740fc7c444d6f9b866ec5f0f0c93b961be6a8
Maria Mikhailova <mariiemik@gmail.com>
2025-05-29 18:19:57 +0300

    delete trash

--- analysis.py
+++ analysis.py
@@ -51,7 +51,7 @@ def plot_comparisons(stats1, stats2):
     plt.show()
 
 """тут когда все методы для пункта 1 сделаем напишем код который соберет все и выведет анализ графа"""
-def print_analysis(graph: dict, directed: bool, large: bool):
+def print_analysis(graph: dict, directed: bool):
     num_of_vertices = number_of_vertices(graph)
     num_of_edges = number_of_edges(graph, directed)
     
--- main.py
+++ main.py
@@ -12,19 +12,17 @@ def extract_subgraph(full_graph, vertices):
 
 def main():
     directed: bool =  False
-    # G = load_graph_from_file("data/undirected/Email-EuAll.txt", directed)
+    # G = load_graph_from_file("data/undirected/Email-EuAll.txt", directed)#!!!используем это для обычных
     
     #для больших графов
-    #G = load_large_graph_from_file("data/very_large_graphs/")
+    #G = load_large_graph_from_file("data/very_large_graphs/") старая версия
 
     print("Section 1------\n")
-    # G, degrees, k = load_graph("data/undirected/Email-EuAll.txt")   #!!!!!используем это для больших
     G = load_graph("data/undirected/Email-EuAll.txt")   #!!!!!используем это для больших
 
-    comp = print_analysis(G, directed, True) ##!!!!!! и передаем это для больших
+    comp = print_analysis(G, directed)
     print("\nSection 1------\n\n")
     print("Section 2------\n")
-    #subgraph = extract_subgraph(G, comp)
     while True:
         user_input = input( """Введите какой метод использовать для выбора ориентиров:  (для остановки введите слово 'stop')\n 1. random_selection\n 2. highest_degree_selection\n 3. best_coverage_selection\n""")
         if user_input.lower() == 'stop':
