ac2e45451614845afc1952ccc7efc0e115997fda
Deymos01 <essenion@gmail.com>
2025-05-30 14:41:16 +0300

    feat: added landmark algorithm into CLI

--- src/cmd/cli/tools.go
+++ src/cmd/cli/tools.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"graph_theory/tools"
 	"path/filepath"
 	"runtime"
 )
@@ -12,4 +14,23 @@ func getFilepath(graphType, graphName string) string {
 
 	filePath := filepath.Clean(projectRoot) + "/" + graphType + "/" + graphName
 	return filePath
+}
+
+func getLandmarkFilepath(graphName, alg string) (string, error) {
+	_, currFile, _, _ := runtime.Caller(0)
+	projectRoot := filepath.Join(filepath.Dir(currFile), "..", "..")
+	projectRoot = filepath.Join(projectRoot, "graph", "landmarkAlgo", "datasets")
+
+	graphName = tools.GetFileNameWithoutExt(graphName)
+
+	filePath := filepath.Clean(projectRoot) + "/" + graphName
+	if alg == "1" {
+		filePath += "-random.txt"
+	} else if alg == "2" {
+		filePath += "-random-paths.txt"
+	} else {
+		return "", fmt.Errorf("unexpected algorithm: %s", alg)
+	}
+
+	return filePath, nil
 }
