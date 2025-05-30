20b4db8fead311bd2043e59ca5795bb05be59a9e
hikkmind <liferisinghigh@yandex.ru>
2025-05-30 12:33:27 +0300

    chore(backend): union handlers

---
+++ backend/handlers/http_handler.go
@@ -0,0 +1,80 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/HikkMind/graph/middleware"
+	"github.com/gorilla/mux"
+)
+
+func GetClusteringInfo(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+	filename := vars["datasetname"] + ".msgpack"
+	w.WriteHeader(http.StatusOK)
+	w.Write(middleware.GenerateClustering(middleware.ReadGraph(storageRoot + filename)))
+
+}
+
+func GetDegreeDistribution(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+	filename := vars["datasetname"] + ".msgpack"
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(middleware.GenerateDegrees(middleware.ReadGraph(storageRoot + filename)))
+}
+
+func GetDistanceEstimation(w http.ResponseWriter, r *http.Request) {
+
+	type GetDistance struct {
+		Method     string `json:"method" msgpack:"method"`
+		SampleSize int    `json:"sampleSize" msgpack:"sampleSize"`
+	}
+
+	vars := mux.Vars(r)
+	filename := vars["datasetname"] + ".msgpack"
+
+	var req GetDistance
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, "Ошибка разбора JSON", http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(middleware.GenerateDistances(middleware.ReadGraph(storageRoot+filename), req.Method, req.SampleSize))
+
+}
+
+func GetGeneralProperties(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+	filename := vars["datasetname"] + ".msgpack"
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(middleware.GenerateProperties(middleware.ReadGraph(storageRoot + filename)))
+}
+
+func GetRobustnessComponent(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+	filename := vars["datasetname"] + ".msgpack"
+
+	type GetRobustness struct {
+		Percentage int    `json:"xPercent" msgpack:"xPercent"`
+		Method     string `json:"removalMethod" msgpack:"removalMethod"`
+	}
+
+	var req GetRobustness
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, "Ошибка разбора JSON", http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(middleware.GenerateRobustness(middleware.ReadGraph(storageRoot+filename), req.Method, req.Percentage))
+
+}
