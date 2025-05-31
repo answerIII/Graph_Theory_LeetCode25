901d76abcc89b193d61f752a3ee253839d702064
kabopok <mishavvvsweater@gmail.com>
2025-05-31 12:07:39 +0300

    first attempt

---
+++ NumberOfProvinces/task_2039/Main.java
@@ -0,0 +1,45 @@
+package org.example;
+
+import java.util.*;
+
+class Solution {
+    public int networkBecomesIdle(int[][] edges, int[] patience) {
+        List<List<Integer>> graph = new ArrayList<>();
+        List<Integer> lengths = new ArrayList<>();
+        for (int i = 0; i < patience.length; ++i) {
+            graph.add(new ArrayList<>());
+        }
+        for (int[] edge : edges) {
+            graph.get(edge[0]).add(edge[1]);
+            graph.get(edge[1]).add(edge[0]);
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(0);
+        lengths.set(0,0);
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            for (int neighbor : graph.get(node)) {
+                if (lengths.get(node)== 0){
+                    lengths.set(neighbor,lengths.get(node)+1);
+                }
+            }
+        }
+
+        int maxTime = 0;
+        for (int i = 0; i < patience.length; ++i) {
+            int curr = 2 * lengths.get(i);
+            if (curr - patience[i] > 0) {curr+=curr - patience[i];}
+            maxTime = Math.max(maxTime,curr);
+        }
+        return maxTime;
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        Solution s=new Solution();
+    }
+
+}
