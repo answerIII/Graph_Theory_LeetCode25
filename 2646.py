ce6b16eddf209eca13cb9f079d8eaddfa0819a3e
sofyashumskikh <shumskikhsofya@gmail.com>
2025-06-02 14:00:00 +0300

    2.0

--- 2646.py
+++ 2646.py
@@ -9,40 +9,57 @@ class Solution(object):
         :type trips: List[List[int]]
         :rtype: int
         """
-        graph = defaultdict(list)
+        graph = [[] for _ in range(n)]
         for u, v in edges:
             graph[u].append(v)
             graph[v].append(u)
 
+        # Оптимизированный подсчет посещений узлов
         node_counts = [0] * n
-        for start, stop in trips:
-            q = deque([(start, [start])])
+        for start, end in trips:
+            # BFS с отслеживанием пути через parent-указатели
+            parent = [-1] * n
+            q = deque([start])
+            parent[start] = start  # Маркер для корня
+            
             while q:
-                curr, path = q.popleft()
-                if curr == stop:
-                    for node in path:
-                        node_counts[node] += 1
+                curr = q.popleft()
+                if curr == end:
                     break
                 for neighbor in graph[curr]:
-                    if neighbor not in path:
-                        q.append((neighbor, path + [neighbor]))
+                    if parent[neighbor] == -1:
+                        parent[neighbor] = curr
+                        q.append(neighbor)
+            
+            # Восстанавливаем путь и считаем узлы
+            node = end
+            while node != parent[node]:
+                node_counts[node] += 1
+                node = parent[node]
+            node_counts[start] += 1  # Не забываем стартовый узел
 
-        def DFS(node, parent, can_halve):
-            non_halve_price = price[node] * node_counts[node]
-            halve_price = (price[node] // 2) * node_counts[node]
-
+        # Оптимизированная DFS с мемоизацией
+        memo = {}
+        def dfs(node, parent, can_halve):
+            key = (node, can_halve)
+            if key in memo:
+                return memo[key]
+            
+            full_price = price[node] * node_counts[node]
+            half_price = (price[node] // 2) * node_counts[node] if can_halve else float('inf')
+            
             for neighbor in graph[node]:
                 if neighbor != parent:
-                    n_h_p, h_p = DFS(neighbor, node, True)
-                    non_halve_price += min(h_p, n_h_p)
+                    neighbor_full, neighbor_half = dfs(neighbor, node, True)
+                    full_price += min(neighbor_full, neighbor_half)
                     if can_halve:
-                        halve_price += n_h_p
-                    else:
-                        halve_price = float('inf')
-            return non_halve_price, halve_price
+                        half_price += dfs(neighbor, node, False)[0]
+            
+            memo[key] = (full_price, half_price)
+            return memo[key]
 
-        non_halve, halve = DFS(0, -1, True)
-        return min(non_halve, halve)
+        full, half = dfs(0, -1, True)
+        return min(full, half)
 
 """
 n = 4
