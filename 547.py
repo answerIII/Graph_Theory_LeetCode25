1712eb507078da6caf134f20e875754be8c7e9f0
sofyashumskikh <shumskikhsofya@gmail.com>
2025-06-02 14:00:00 +0300

    3.0

--- 547.py
+++ 547.py
@@ -1,27 +1,22 @@
 class Solution(object):
     def findCircleNum(self, isConnected):
-        """
-        :type isConnected: List[List[int]]
-        :rtype: int
-        """
         n = len(isConnected)
         count = 0
-        visited = [False] * n
-
-        stack = []
+        visited = bytearray(n)
+        
         for i in range(n):
             if not visited[i]:
-                stack.append(i)
                 count += 1
-                while stack:
-                    city = stack.pop()
-                    visited[city] = True
+                visited[i] = 1
+                queue = [i]
+                while queue:
+                    city = queue.pop()
                     for neighbor in range(n):
                         if isConnected[city][neighbor] and not visited[neighbor]:
-                            stack.append(neighbor)
-                            visited[neighbor] = True
+                            queue.append(neighbor)
+                            visited[neighbor] = 1
         return count
-
+    
 """
 isConnected = [[1, 1, 0], [1, 1, 0], [0, 0, 1]]
 s = Solution()
