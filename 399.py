00a4ea1120868c989e1d3a3a7b17dcc297ae20fa
sofyashumskikh <shumskikhsofya@gmail.com>
2025-06-02 14:00:00 +0300

    3.0

--- 399.py
+++ 399.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 class Solution(object):
     def calcEquation(self, equations, values, queries):
         """
@@ -10,29 +12,36 @@ class Solution(object):
         for (A,B), value in zip(equations,values):
             if A not in graph:
                 graph[A] = {}
+            graph[A][B] = value
             if B not in graph:
                 graph[B] = {}
-            graph[A][B] = value
             graph[B][A] = 1./value
 
         def DFS(start,stop,visited):
             if start == stop:
                 return 1.
             visited.add(start)
-            for neighbor, value in graph[start].items():
+            neighbors = graph[start]
+            for neighbor in neighbors:
                 if neighbor not in visited:
-                    result = DFS(neighbor,stop,visited)
-                    if result != -1.:
-                        return value * result
-            return -1.
+                    res = DFS(neighbor, stop, visited)
+                    if res != -1.0:
+                        return neighbors[neighbor] * res
+            return -1.0
 
-        return [DFS(C,D,set()) if C in graph and D in graph else -1. for C, D in queries]
+        result = []
+        append_result = result.append
+        for C, D in queries:
+            if C in graph and D in graph:
+                append_result(DFS(C, D, set()))
+            else:
+                append_result(-1.0)
+        return result
 
-"""
+
 equations = [["a","b"],["b","c"]]
 values = [2.0,3.0]
 queries = [["a","c"],["b","a"],["a","e"],["a","a"],["x","x"]]
 solution = Solution()
 result = solution.calcEquation(equations, values, queries)
 print(result)
-"""
