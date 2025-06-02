a880a73e0a4f480fc8f3f364b23a931a2d960c2e
sofyashumskikh <shumskikhsofya@gmail.com>
2025-06-02 14:00:00 +0300

    2.0

--- 797.py
+++ 797.py
@@ -6,25 +6,15 @@ class Solution(object):
         """
         n = len(graph)
         target = n - 1
-        paths = []
+        result = []
 
-        def DFS(node, path):
-            path.append(node)
-
+        def dfs(node, path):
             if node == target:
-                paths.append(path[:])
-            else:
-                for neighbor in graph[node]:
-                    DFS(neighbor,path)
+                result.append(path + [node])
+                return
+            
+            for neighbor in graph[node]:
+                dfs(neighbor, path + [node])
 
-            path.pop()
-
-        DFS(0,[])
-        return paths
-
-"""
-graph = [[4,3,1],[3,2,4],[3],[4],[]]
-solution = Solution()
-result = solution.allPathsSourceTarget(graph)
-print(result)
-"""
+        dfs(0, [])
+        return result
